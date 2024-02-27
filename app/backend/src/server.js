a9f0bad16859d36d4374c7cc761982ab677faa7b
Ved Sampath <vedsampath@lawn-128-61-39-38.lawn.gatech.edu>
2024-02-26 22:45:52 -0500

    added s3 bucket functionality and restructured code, still need to connect front-end and back-end

--- app/backend/src/server.js
+++ app/backend/src/server.js
@@ -5,11 +5,6 @@ const app = express();
 // Enable CORS for all routes and origins
 app.use(cors());
 
-// Your route definitions and other middleware
-app.post('/upload-pdf', (req, res) => {
-  // Handle the PDF upload
-});
-
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {

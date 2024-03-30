4089d1a69064a457604f837cbeb7d871e8dcdd32
vsuresh36 <vishnu.g.suresh@gmail.com>
2024-03-30 12:11:27 -0400

    report summarize + refactor gpt call

--- app/backend/src/upload/chatGPTService.js
+++ app/backend/src/upload/chatGPTService.js
@@ -5,19 +5,16 @@ const openai = new OpenAI({
   apiKey: process.env.OPEN_AI_SECRET_KEY,
 });
 
-const callChatGPTAPI = async (extractedText) => {
+const callChatGPTAPI = async (prompt) => {
     try {
-        const prompt = "Listed below is a patient's blood work. The JSON format will include an array of entries, called 'testsbydate' (could have only one), where entries each include and are split up by a unique 'effectiveDateTime' key associated with a value of the date that the blood test was conducted of the form YYYY-MM-DD. Each entry should contain an array of sub-entries, called 'test' (may only have one) for each and that contains 'bloodtestname' and 'value' (required), that also contains 'range' and 'unit' if it's there, otherwise just set the value to string, 'None'. Your output should be a JSON string only and nothing else";
         const response = await openai.chat.completions.create({
-            model: "gpt-4-0125-preview",
-            messages: [{ role: "user", content: prompt + extractedText }],
+            model: "gpt-4-turbo-preview",
+            messages: [{ role: "user", content: prompt }],
             temperature: 0,
         });
 
-        let chatResponse = response.choices[0].message.content;
-        chatResponse = chatResponse.replace(/```json|```/g, '').trim()
-        const chatGPTResponseJson = JSON.parse(chatResponse);
-        return chatGPTResponseJson;
+        console.log(response)
+        return response
     } catch (error) {
         throw error;
     }
--- app/frontend/app/app/report/[id]/page.tsx
+++ app/frontend/app/app/report/[id]/page.tsx
@@ -13,6 +13,41 @@ async function getReport(uniqueUserId: any, date: any) {
   return data?.data as any[];
 }
 
+const generatePrompt = (contextText: string) => {
+  const prompt = `${`
+  You are a medical practitioner extremely proficient in bloodwork.
+  Assume your patient is a high school student.
+  You wil be given a blood report in similar format to a json string.
+  Act as if you are speaking to a patient explaining their bloodwork to them.
+  Answer in an objective manner and err on the side of caution.
+  Do not suggest any treatment options, simply summarize their blood report."`}
+
+  Bloodwork Context:
+  ${contextText}
+
+  Question: Summarize the following blood report and answer as a nurse would
+  explain the following results to a high school student. Emphasize the most important tests.
+`;
+
+  return prompt;
+};
+
+const generateAnswer = async (prompt: string) => {
+	console.log('generating answer')
+
+	const res = await fetch(`http://localhost:3001/summarize?prompt=${prompt}`);
+
+	if (res.status !== 200) {
+		console.log('error')
+	} else {
+		console.log('inb here')
+		const data = await res.json();
+		console.log(data)
+
+		return data.choices[0].message.content
+	}
+};
+
 interface Data {
   dateTimeType: string;
   uniqueUserId: string;
@@ -38,6 +73,13 @@ export default async function ReportPage({ params }: any) {
   const day = date.split("-")[2];
 
   const reportData = report.data;
+  console.log(JSON.stringify(reportData))
+
+	const prompt = generatePrompt(String(JSON.stringify(reportData)));
+	console.log(prompt)
+
+	const gpt_out = await generateAnswer(prompt);
+  console.log(gpt_out)
 
   return (
     <main>
@@ -64,6 +106,11 @@ export default async function ReportPage({ params }: any) {
         <div className="mt-8">
           <p className="opacity-50">Data</p>
           <ReportTable isFhir={type === "fhir"} reportData={reportData} />
+        </div>
+
+        <div className="mt-8">
+          <p className="opacity-50">Blood Report Summary</p>
+          <p>{gpt_out}</p>
         </div>
       </div>
     </main>

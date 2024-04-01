672bb0de96ea4276f78732ceeb160af15092c88e
vsuresh36 <vishnu.g.suresh@gmail.com>
2024-04-01 14:12:24 -0400

    Reversed Timeline

--- app/frontend/app/app/_components/timeline/Timeline.tsx
+++ app/frontend/app/app/_components/timeline/Timeline.tsx
@@ -14,13 +14,19 @@ export default async function Timeline() {
   const userName = (await getIdToken()).name;
   const uniqueUserId = (await getIdToken()).sub;
 
+  // Assuming getItems returns an array of items
   // const data = await getItems(uniqueUserId);
   const data = await getItems("kp_f85ba560eb6346ccb744778f7c8d769e");
+
+  // Reverse the order of the data array
+  const reversedData = data?.reverse();
+
   return (
     <div className="mt-16 border-l pl-[0.925rem] dark:border-l-stone-900">
-      {data?.map((report) => {
+      {reversedData?.map((report) => {
         return <Report key={report.dateTimeType} report={report} />;
       })}
     </div>
   );
 }
+
